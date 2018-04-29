646a3b9451ad2c861ec557a71246429e483600ad
Autioch <autioch+github@gmail.com>
2018-04-29 22:22:55 +0200

    Docs app.

--- src/macro/app/src/index.js
+++ src/macro/app/src/index.js
@@ -1,18 +1,14 @@
-import createStore from 'pipe-and-gauge/src/createStore';
+import { createApp } from 'pipe-and-gauge';
 import actions from './actions';
 import initialState from './initialState';
 import App from './App';
 import './styles.css';
-import React from 'react';
-import { render } from 'react-dom';
 
 /* data */
 import { categories, macros, tags } from './data';
 import icons from './macroIcons';
 
-const store = createStore(actions, initialState);
-
-store.subscribe(({ state }) => render(<App state={state} store={store}/>, document.getElementById('root')));
+const store = createApp(actions, initialState, App, document.getElementById('root'));
 
 setTimeout(() => store.setData({
   categories,
